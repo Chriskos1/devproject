3495c6ad99db0d867882a7075ffff448facc6e3f
Chris <kostine@et.esiea.fr>
2022-05-30 23:39:32 +0200

    Update program.js
    ->figcaption part works

--- program.js
+++ program.js
@@ -7,28 +7,32 @@ function additem(){
     document.getElementById("pictures_section").appendChild(para);
 
 
-    var elem = document.createElement("img");
-    document.getElementById("placehere").appendChild(elem);
-    
-
-    const image_input = document.querySelector("addpicture");image_input.addEventListener("change", function() {
-  const reader = new FileReader();
-  reader.addEventListener("load", () => {
-    const uploaded_image = reader.result;
-    document.querySelector("pictures_section").style.backgroundImage = `url(${uploaded_image})`;
-      });
-  reader.readAsDataURL(this.files[0]);
-});
 }
-
+    const image_input = document.querySelector("#addpicture");image_input.addEventListener("change", function() {
+      const reader = new FileReader();
+      reader.addEventListener("load", () => {
+        const uploaded_image = reader.result;
+        document.querySelector("#pictures_section").style.backgroundImage = `url(${uploaded_image})`;
+      });
+      reader.readAsDataURL(this.files[0]);
+    });
 
 function deleteitem(){
 
     var element = document.getElementById("delete").value;
-
-    if(element == document.getElementById().value){
+    var Amsterdam = document.getElementById("Amsterdam").value
+    var Brussel = document.getElementById("Brussel").value
+    var Paris = document.getElementById("Paris").value
+    var Roma = document.getElementById("Roma").value
+    var Vienna = document.getElementById("Vienna").value
+    var Prague = document.getElementById("Prague").value
+    var Madrid = document.getElementById("Madrid").value
+    var Bucharest =document.getElementById("Bucharest").value
+    if(element == Amsterdam || element == Brussel || element == Paris || element == Roma || element == Vienna || element == Prague || element == Madrid || element == Bucharest ){
 
 	element.parentNode.removeChild(element);
 	}
+
+
 
 }
